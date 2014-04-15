5391119bb2d4989c21ff9ba3d4df5ed4a9ae383a
romank <roman.khazankin@agfa.com>
2014-04-16 00:33:18 +0200

    ...

--- dcm4chee-wizard-war/src/main/java/org/dcm4chee/wizard/model/GenericConfigNodeModel.java
+++ dcm4chee-wizard-war/src/main/java/org/dcm4chee/wizard/model/GenericConfigNodeModel.java
@@ -1,11 +1,17 @@
 package org.dcm4chee.wizard.model;
 
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
 import org.apache.wicket.model.IModel;
 import org.dcm4che3.conf.api.ConfigurationException;
+import org.dcm4che3.conf.api.DicomConfiguration;
 import org.dcm4che3.conf.api.generic.ReflectiveConfig;
-import org.dcm4che3.conf.api.generic.ReflectiveConfig.ConfigNode;
 import org.dcm4che3.conf.api.generic.adapters.ReflectiveAdapter;
+import org.dcm4che3.net.ApplicationEntity;
+import org.dcm4che3.net.Device;
 import org.dcm4che3.net.DeviceExtension;
+import org.dcm4che3.net.DeviceInfo;
 import org.dcm4chee.xds2.conf.XdsRepository;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
@@ -19,7 +25,7 @@ import com.fasterxml.jackson.databind.util.NameTransformer;
 public class GenericConfigNodeModel<T extends DeviceExtension> implements IModel<String> {
 
     private static final long serialVersionUID = -8533244684805795443L;
-    private ConfigNode node;
+    private Map<String,Object> node;
     ObjectMapper om;
     ReflectiveConfig rconfig;
     ReflectiveAdapter<T> ad;
@@ -37,7 +43,121 @@ public class GenericConfigNodeModel<T extends DeviceExtension> implements IModel
         this.fieldName = fieldName;
 
         om = new ObjectMapper();
-        rconfig = new ReflectiveConfig(null, null);
+        rconfig = new ReflectiveConfig(null, new DicomConfiguration() {
+            
+            @Override
+            public void unregisterAETitle(String aet) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void sync() throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void removeDevice(String name) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void removeCertificates(String ref) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public boolean registerAETitle(String aet) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return false;
+            }
+            
+            @Override
+            public boolean purgeConfiguration() throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return false;
+            }
+            
+            @Override
+            public void persistCertificates(String ref, X509Certificate... certs) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void persist(Device device) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void merge(Device device) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public String[] listRegisteredAETitles() throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public String[] listDeviceNames() throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public DeviceInfo[] listDeviceInfos(DeviceInfo keys) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public <T> T getDicomConfigurationExtension(Class<T> clazz) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public Device findDevice(String name) throws ConfigurationException {
+                return new Device(name);
+            }
+            
+            @Override
+            public X509Certificate[] findCertificates(String dn) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public ApplicationEntity findApplicationEntity(String aet) throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public String deviceRef(String name) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public boolean configurationExists() throws ConfigurationException {
+                // TODO Auto-generated method stub
+                return false;
+            }
+            
+            @Override
+            public void close() {
+                // TODO Auto-generated method stub
+                
+            }
+        });
         ad = new ReflectiveAdapter<T>((Class<T>) confObj.getClass());
 
     }
@@ -51,9 +171,10 @@ public class GenericConfigNodeModel<T extends DeviceExtension> implements IModel
 
         try {
             // serialize to confignode
-            ConfigNode cn = ad.serialize(confObj, rconfig, null);
+            Map<String,Object> cn = ad.serialize(confObj, rconfig, null);
 
             // serialize to pretty json
+            //return om.writeValueAsString(cn.get(fieldName));
             return om.writerWithDefaultPrettyPrinter().writeValueAsString(cn.get(fieldName));
 
         } catch (Exception e) {
@@ -66,10 +187,10 @@ public class GenericConfigNodeModel<T extends DeviceExtension> implements IModel
         try {
 
             // deserialize json
-            ConfigNode propNode = om.readValue(arg0, ConfigNode.class);
+            Map<String,Object> propNode = om.readValue(arg0, Map.class);
 
             // serialize full obj
-            ConfigNode confNode = ad.serialize(confObj, rconfig, null);
+            Map<String,Object> confNode = ad.serialize(confObj, rconfig, null);
 
             // set field
             confNode.put(fieldName, propNode);
---
+++ dcm4chee-wizard-war/src/test/java/org/dcm4chee/wizard/UnwrappingSerializationTest.java
@@ -0,0 +1,152 @@
+package org.dcm4chee.wizard;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.dcm4che3.net.Device;
+import org.dcm4chee.wizard.model.GenericConfigNodeModel;
+import org.dcm4chee.xds2.conf.XCAInitiatingGWCfg;
+import org.dcm4chee.xds2.conf.XdsBrowser;
+import org.dcm4chee.xds2.conf.XdsRegistry;
+import org.dcm4chee.xds2.conf.XdsRepository;
+import org.dcm4chee.xds2.conf.XCAInitiatingGWCfg.GatewayReference;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class UnwrappingSerializationTest {
+
+    private static final String[] MIME_TYPES2 = new String[] { "application/xml", "application/dicom", "application/pdf", "text/plain",
+            "text/xml" };
+    private String[] AFFINITY_DOMAIN = { "1.2.3.4.5" };
+
+    private XdsRepository createRepo() throws Exception {
+        // create registry which will be referenced
+        Device regd = new Device("RegDevice");
+
+        Device srcd = new Device("source_device");
+
+        XdsRepository rep = new XdsRepository();
+
+        rep.setApplicationName("AppNNName");
+        rep.setRepositoryUID("1.2.3");
+        rep.setAcceptedMimeTypes(MIME_TYPES2);
+        rep.setSoapLogDir(null);
+        rep.setCheckMimetype(true);
+        rep.setAllowedCipherHostname("*");
+        rep.setLogFullMessageHosts(new String[] {});
+        rep.setRetrieveUrl("http://retrieve");
+        rep.setProvideUrl("http://provide");
+        rep.setForceMTOM(true);
+
+        // reference registry
+        Map<String, Device> deviceBySrcUid = new HashMap<String, Device>();
+
+        deviceBySrcUid.put("3.4.5", srcd);
+        rep.setSrcDevicebySrcIdMap(deviceBySrcUid);
+
+        XdsRepository repo = rep;
+        return repo;
+    }
+
+    
+    private XCAInitiatingGWCfg createInitGw(){
+        XCAInitiatingGWCfg initGW = new XCAInitiatingGWCfg();
+
+        initGW.setApplicationName("appn");
+        initGW.setHomeCommunityID("1.2.3");
+        initGW.setSoapLogDir("/dir");
+        initGW.setLocalPIXConsumerApplication("pixapp");
+        initGW.setRemotePIXManagerApplication("pixmgrapp");
+        initGW.setAsync(false);
+        initGW.setAsyncHandler(true);
+
+        // registry
+        Device regd = new Device("registry_device_rgw");
+        initGW.setRegistry(regd);
+
+        // repos
+
+        Device repod1 = new Device("repo_device_1");
+        Device repod2 = new Device("repo_device_2");
+
+        Map<String, Device> repDevices = new HashMap<String, Device>();
+        repDevices.put("123", repod1);
+        repDevices.put("456", repod2);
+
+        initGW.setRepositoryDeviceByUidMap(repDevices);
+
+
+        Device rgwd2 = new Device("rgw_device_2");
+
+        GatewayReference gwr1 = new GatewayReference();
+        gwr1.setAffinityDomain("1.2.3.4.5");
+        gwr1.setRespondingGWdevice(repod1);
+
+        GatewayReference gwr2 = new GatewayReference();
+        gwr2.setAffinityDomain("10.20.30.40.50");
+        gwr2.setRespondingGWdevice(rgwd2);
+
+        Map<String, GatewayReference> gws = new HashMap<String, GatewayReference>();
+        gws.put("1001", gwr1);
+        gws.put("2002", gwr2);
+
+        initGW.setRespondingGWByHomeCommunityIdMap(gws);
+        
+        return initGW;
+    }
+    
+   
+    private XdsRegistry createRegistry() {
+        XdsRegistry registry = new XdsRegistry();
+        registry.setApplicationName("appReg");
+        registry.setAffinityDomain(AFFINITY_DOMAIN);
+        registry.setAffinityDomainConfigDir("${jboss.server.config.dir}/affinitydomain");
+        registry.setAcceptedMimeTypes(MIME_TYPES2);
+        registry.setSoapLogDir(null);
+        registry.setCreateMissingPIDs(false);
+        registry.setCreateMissingCodes(false);
+        registry.setCheckAffinityDomain(true);
+        registry.setCheckMimetype(true);
+        registry.setPreMetadataCheck(false);
+        registry.setRegisterUrl("http://localhost/registryregister");
+        registry.setQueryUrl("http://localhost/registryquery");
+        
+        XdsBrowser xdsBrowser = new XdsBrowser();
+        
+        
+        
+        Set<Device> devs = new HashSet<Device>();
+        devs.add(new Device("abcDev"));
+        devs.add(new Device("another!Dev"));
+        devs.add(new Device("AlsoControlledDev"));
+        xdsBrowser.setControlledDevices(devs);
+        
+        registry.setXdsBrowser(xdsBrowser);
+        return registry;
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSerializeUnwrap() throws Exception {
+
+        XCAInitiatingGWCfg initgw = createInitGw();
+        GenericConfigNodeModel<XCAInitiatingGWCfg> nm = new GenericConfigNodeModel<XCAInitiatingGWCfg>(initgw, "xdsRespondingGateways");
+        System.out.println(nm.getObject());
+        nm.setObject("{\"1301\":{\"xdsAffinityDomain\":\"1.2.5\",\"xdsRespondingGateway\":\"repo_dDDD\"},\"2002\":{\"xdsAffinityDomain\":\"10.20.30.40.50\",\"xdsRespondingGateway\":\"rgw_device_2\"}}");
+        System.out.println(nm.getModifiedConfigObj().getRespondingGWByHomeCommunityIdMap());
+
+        
+        XdsRegistry reg = createRegistry();
+        GenericConfigNodeModel<XdsRegistry> nm1 = new GenericConfigNodeModel<XdsRegistry>(reg, "xdsBrowser");
+        System.out.println(nm1.getObject());
+        nm1.setObject("{\"xdsControlledDevices\" : [ \"BOODev\", \"TheThirdanotherDev\", \"AlsoControlledDev\" ]}");
+        System.out.println(nm1.getModifiedConfigObj().getXdsBrowser().getControlledDevices());
+        
+
+    }
+
+}
