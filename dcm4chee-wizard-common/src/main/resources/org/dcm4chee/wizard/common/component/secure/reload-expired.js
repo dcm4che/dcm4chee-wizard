05ebd59e8b4e3349d52debf53e4517b4ebc5f823
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-17 14:31:41 +0800

    Delete dcm4chee-wizard-common directory

--- dcm4chee-wizard-common/src/main/resources/org/dcm4chee/wizard/common/component/secure/reload-expired.js
+++
@@ -1,7 +0,0 @@
-window.onload = setupReload;
-
-function setupReload() {
-	Wicket.Event.subscribe('/ajax/call/failure', function(jqEvent, attributes, jqXHR, errorThrown, textStatus) {
-		parent.location.reload();
-	});
-}
